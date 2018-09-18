f2089f74534289caafa5adeaac65af4ab6cccc2b
David Guben <davidjg6@gmail.com>
2018-09-18 09:49:42 -0400

    server-side rendering github popular repos by language

--- src/server/index.js
+++ src/server/index.js
@@ -1,41 +1,57 @@
 import express from "express"
 import cors from "cors"
+import React from "react"
 import { renderToString } from "react-dom/server"
-import App from '../shared/App'
-import React from 'react'
+import { StaticRouter, matchPath } from "react-router-dom"
 import serialize from "serialize-javascript"
+import App from '../shared/App'
+import routes from '../shared/routes'
 
 const app = express()
 
 app.use(cors())
-
-// We're going to serve up the public
-// folder since that's where our
-// client bundle.js file will end up.
 app.use(express.static("public"))
 
 app.get("*", (req, res, next) => {
-  const name = 'David'
-  const markup = renderToString(
-    <App data={name} />
-  )
+  const activeRoute = routes.find((route) => matchPath(req.url, route)) || {}
 
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>SSR with RR</title>
-        <script src="/bundle.js" defer></script>
-        <script>window.__INITIAL_DATA__ = ${serialize(name)}</script>
-    </head>
+  const promise = activeRoute.fetchInitialData
+    ? activeRoute.fetchInitialData(req.path)
+    : Promise.resolve()
 
-      <body>
-        <div id="app">${markup}</div>
-      </body>
-    </html>
-  `)
+  promise.then((data) => {
+    const context = { data }
+
+    const markup = renderToString(
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+    )
+
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>SSR with RR</title>
+          <script src="/bundle.js" defer></script>
+          <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+        </head>
+
+        <body>
+          <div id="app">${markup}</div>
+        </body>
+      </html>
+    `)
+  }).catch(next)
 })
 
 app.listen(3000, () => {
   console.log(`Server is listening on port: 3000`)
 })
+
+/*
+  1) Just get shared App rendering to string on server then taking over on client.
+  2) Pass data to <App /> on server. Show diff. Add data to window then pick it up on the client too.
+  3) Instead of static data move to dynamic data (github gists)
+  4) add in routing.
+*/
